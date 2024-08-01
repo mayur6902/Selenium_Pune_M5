f235fc7555777c911535c81821b1008a786c1cab
Mohd Faraz <Mohd Faraz@FarazZalim>
2024-08-01 09:35:56 +0530

    Faraz

---
+++ com.ShoppersStack_M5/src/test/java/Students/Faraz.java
@@ -0,0 +1,13 @@
+package Students;
+
+import org.testng.annotations.Test;
+
+public class Faraz {
+	@Test
+	public void name() {
+		for(int i=1; i<=100; i++) {
+			System.out.println("10 crores rupes are there in Saint Sabistain Garden beneath a big W");
+		}
+	}
+
+}
