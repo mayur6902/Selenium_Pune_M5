4922bf6575f83e859a33fbc9bb6759006fee2747
sirsathe <101626254+sirsathe@users.noreply.github.com>
2024-08-01 09:47:04 +0530

    Pratik

---
+++ com.ShoppersStack_M5/src/test/java/Students/Pratik.java
@@ -0,0 +1,16 @@
+package Students;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.model.Report;
+
+public class Pratik {
+
+	@Test
+	
+	public void myName() {
+		System.out.println("Pratik 2K20");
+	}
+   
+}
